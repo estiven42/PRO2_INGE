c6704809f3401f958bd0f79724313729a58bb93f
esteban209 <estebansanabriacortes@gmail.com>
2024-04-18 18:58:24 -0500

    Ajuste metodo suma

--- src/main/java/co/tarjetaCredito/controladores/Suma.java
+++ src/main/java/co/tarjetaCredito/controladores/Suma.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class Suma {
      public Integer sumaNumeros(Integer num1, Integer num2){
         Integer resultado = num1 + num2;
+        System.out.println("prueba");
         return resultado;
      }
 }
