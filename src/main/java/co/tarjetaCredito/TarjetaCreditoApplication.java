1260dfdc2f8f188e75992df736dd26530d30c69f
esteban209 <estebansanabriacortes@gmail.com>
2024-04-18 18:54:34 -0500

    ajuste de arquitectura

---
+++ src/main/java/co/tarjetaCredito/TarjetaCreditoApplication.java
@@ -0,0 +1,20 @@
+package co.tarjetaCredito;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+/**
+ * author Vinni 2023
+ */
+@SpringBootApplication
+@Log4j2
+public class TarjetaCreditoApplication {
+    public static void main(String[] args) {
+        System.out.println("proyecto base");
+
+    }
+
+}
---
+++ src/main/java/co/tarjetaCredito/controladores/Suma.java
@@ -0,0 +1,10 @@
+package co.tarjetaCredito.controladores;
+
+import java.io.*;
+
+public class Suma {
+     public Integer sumaNumeros(Integer num1, Integer num2){
+        Integer resultado = num1 + num2;
+        return resultado;
+     }
+}
