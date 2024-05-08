a13df0153b9d9961cf4dba3220e056600a8d6222
esteban20983 <159496540+esteban20983@users.noreply.github.com>
2024-05-08 11:33:24 -0500

    crear controlador y pagina de inicio

--- src/main/java/co/tarjetaCredito/controladores/EmpleadoControl.java
+++ src/main/java/co/tarjetaCredito/controladores/EmpleadoControl.java
@@ -1,9 +1,32 @@
  package co.tarjetaCredito.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
 
-@Controller
+import co.tarjetaCredito.entidades.Empleado;
+import co.tarjetaCredito.servicios.EmpleadoServ;
+
+@RestController
+@Transactional
+@RequestMapping({ "/empleado" })
  public class EmpleadoControl {
 
-    
+    @Autowired
+    private EmpleadoServ empleadoServ;
+
+    @PostMapping({ "/registrar" })
+	public ResponseEntity<?> guardarEmpleado(@RequestBody Empleado emp) {
+
+        this.empleadoServ.saveEmpleado(emp);
+        
+
+        return ResponseEntity.badRequest().body("Guardado");
+    }
+
 }
